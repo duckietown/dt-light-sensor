3b6470c0cd16e9dda80d89ac706ca1fd889ffbce
David <muedavid@student.ethz.ch>
2020-04-02 10:58:02 +0200

    update to DTROS and changed a lot of other things:
    deleted unnecessary things in the code to make the code more structured and clean
    
    callibration node:
    Tool to recallibrate if callibration wasn't good
    If callibration has already be done we can decide if we want to recallibrate everything

--- packages/light_sensor/src/light_sensor_node.py
+++ packages/light_sensor/src/light_sensor_node.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import LightSensor
 import time
-# from Adafruit_GPIO import I2C
 import RPi.GPIO as GPIO
 import Adafruit_TCS34725
-# import smbus
 import yaml
 import os.path
-# from duckietown_utils import get_duckiefleet_root
+from duckietown_msgs.msg import LightSensor
+from duckietown import DTROS
 
 
-class LightSensorNode(object):
+class LightSensorNode(DTROS):
+    """Node handling the measurment of light.
+        measures the light in the room and publish it.
 
-    def __init__(self):
-        # Get node name and vehicle name
-        self.node_name = rospy.get_name()
-        self.veh_name = self.node_name.split("/")[1]
-        self.debug = False
+        Publishers:
+           ~sensor_data (:obj:`LightSensor`): Publishes measured intesitiy of red, green and blue, the calculated lux and the real_lux value that takes 
+           callibration into account. It publishes also the temperature of the light. 
+    """
+    
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(LightSensorNode, self).__init__(node_name=node_name)
+
+        # Add the node parameters to the parameters dictionary and load their default values
+        self.parameters['~default_mult'] = None
+        self.parameters['~default_offset']=None
+        self.parameters['~frequency']=None
+        self.updateParameters()
+
+        self.veh_name = rospy.get_namespace().strip("/")
+        
         # GPIO setup
         # Choose BCM or BOARD numbering schemes
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(18, GPIO.OUT)
-        # turn off LED
         GPIO.output(18, GPIO.LOW)
 
         # Set integrationtime and gain
@@ -31,19 +42,29 @@ class LightSensorNode(object):
             integration_time=Adafruit_TCS34725.TCS34725_INTEGRATIONTIME_700MS,
             gain=Adafruit_TCS34725.TCS34725_GAIN_1X)
 
-        # Set parameter
+        #define file path
+        self.filepath = '/data/config/calibrations/light-sensor/' + self.veh_name + ".yaml"
+
+        #define parameter
+        self.mult = 0
+        self.offset = 0
+        self.default_mult=self.parameters['~default_mult'] #default value = 1
+        self.default_offset= self.parameters['~default_offset'] #default value = 0
+        #set parametervalue
         self.readParamFromFile()
-        # Set local gain using yam
-        self.mult = self.setup_parameter("~mult", 1)
-        self.offset = self.setup_parameter("~offset", 1)
+        
+        #define time between evaluations in sec. 
+        self.timesensor = 1/self.parameters['~frequency'] #value = 1: every second one measurment
+
 
         # ROS-Publications
         self.msg_light_sensor = LightSensor()
-        self.sensor_pub = rospy.Publisher(
-            '~sensor_data', LightSensor, queue_size=1)
-        get_lux_timer = rospy.Timer(rospy.Duration(secs=1), self.get_lux)
-
-    def get_lux(self, timerevent):
+        self.sensor_pub = self.publisher('~sensor_data', LightSensor, queue_size=1)
+        while not rospy.is_shutdown():
+            self.get_lux()
+            rospy.sleep(rospy.Duration.from_sec(self.timesensor))
+        
+    def get_lux(self):
         # Read R, G, B, C color data from the sensor.
         r, g, b, c = self.tcs.get_raw_data()
         # Calulate color temp
@@ -51,22 +72,11 @@ class LightSensorNode(object):
         # Calculate lux and multiply it with gain
         lux = Adafruit_TCS34725.calculate_lux(r, g, b)
         real_lux = self.mult * lux + self.offset
-        # Calculate lux out of RGB measurements.
-        if self.debug:
-            print("temp [k]= ", temp)
-            print("r :", r)
-            print("g :", g)
-            print("b :", b)
-            print("c :", c)
-            print("lux = ", lux)
-            print("real_lux: ", real_lux)
 
         # Publish to topic
-
-        # TODO: add other things to header
+        #header
         self.msg_light_sensor.header.stamp = rospy.Time.now()
-        self.msg_light_sensor.header.frame_id = rospy.get_namespace()[
-            1:-1]  # splicing to remove /
+        self.msg_light_sensor.header.frame_id = rospy.get_namespace()[1:-1] 
 
         self.msg_light_sensor.r = r
         self.msg_light_sensor.g = g
@@ -77,47 +87,25 @@ class LightSensorNode(object):
         self.msg_light_sensor.temp = temp
         self.sensor_pub.publish(self.msg_light_sensor)
 
-    def getFilePath(self, name):
-        return ('/data/config/calibrations/light-sensor/' + name + ".yaml")
+    
 
     def readParamFromFile(self):
-        # Check file existance
-        fname = self.getFilePath(self.veh_name)
         # Use default.yaml if file doesn't exsit
-        if not os.path.isfile(fname):
-            rospy.logwarn("[%s] %s does not exist. Using default values" % (
-                self.node_name, fname))
+        if not os.path.isfile(self.filepath):
+            rospy.logwarn("[%s] %s does not exist. Using default values" % (self.node_name, self.filepath))
+            self.mult = self.default_mult
+            self.offset = self.default_offset
+            
         else:
-            with open(fname, 'r') as in_file:
-                try:
-                    yaml_dict = yaml.load(in_file)
-                except yaml.YAMLError as exc:
-                    rospy.logfatal("[%s] YAML syntax error. File: %s fname. Exc: %s" % (
-                        self.node_name, fname, exc))
-                    rospy.signal_shutdown("light sensor exiting")
-                    return
-
-                # Set parameters using value in yaml file
-                if yaml_dict is None:
-                    # Empty yaml file
-                    return
-                for param_name in ["mult", "offset"]:
-                    param_value = yaml_dict.get(param_name)
-                    if param_value is not None:
-                        rospy.set_param("~"+param_name, param_value)
-                    else:
-                        # Skip if not defined, use default value instead.
-                        pass
-
-    def setup_parameter(self, param_name, default_value):
-        value = rospy.get_param(param_name, default_value)
-        # Write to parameter server for transparency
-        rospy.set_param(param_name, value)
-        rospy.loginfo("[%s] %s = %s " % (self.node_name, param_name, value))
-        return value
+            with open(self.filepath, 'r') as in_file:
+                yaml_dict = yaml.load(in_file)
+                self.mult = yaml_dict["mult"]
+                self.offset = yaml_dict["offset"]
+                
+        rospy.loginfo("[%s] from callibration %s = %s " % (self.node_name,"mult", self.mult))
+        rospy.loginfo("[%s] from callibration %s = %s " % (self.node_name,"offset", self.offset))
 
 
 if __name__ == '__main__':
-    rospy.init_node('light_sensor_node', anonymous=False)
-    light_sensor_node = LightSensorNode()
+    node = LightSensorNode(node_name='light_sensor_node')
     rospy.spin()
